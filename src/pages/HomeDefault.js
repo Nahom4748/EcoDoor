abe075be7c1697567ee6abbfbec81cbdfb33a4c2
Nahom4748 <nahom4748@gmail.com>
2025-07-15 11:42:53 -0700

    pushing new

--- src/pages/HomeDefault.js
+++ src/pages/HomeDefault.js
@@ -4,21 +4,17 @@ import WhyChooseOne from "../components/why-choose-us/WhyChooseOne";
 import ServiceOne from "../components/service/ServiceOne";
 // import ProjectThree from "../components/project/ProjectThree.js";
 import WorkProcessOne from "../components/work-process/WorkProcessOne";
-import FaqOne from "../components/faq/FaqOne";
 import FaqTwo from "../components/faq/FaqTwo";
 import FooterOne from "../common/footer/FooterOne";
 import HeaderTwo from "../common/header/HeaderTwo";
-import HeroOne from "../components/hero/HeroOne";
 
 import HeroTwo from "../components/hero/HeroTwo";
 import ServiceTwo from "../components/service/ServiceTwo";
 import AboutThree from "../components/about/AboutThree";
 // import ServiceThree from "../components/service/ServiceThree";
-import TeamTwo from "../components/team/TeamTwo.js";
 import TestimonialTwo from "../components/testimonial/TestimonialTwo.js";
 import OurCustomers from "../components/service/OurCustomers.js";
 import Unique from "../components/service/Unique.js";
-import ProjectThree from "../components/project/ProjectThree.js";
 
 const HomeDefault = () => {
   return (
@@ -32,16 +28,14 @@ const HomeDefault = () => {
       <Unique />
 
       <ServiceOne />
-      {/* <ProjectOne /> */}
-      {/* <ProjectThree /> */}
+   
       <WorkProcessOne />
       <OurCustomers />
       {/* <TeamTwo /> */}
       <FaqTwo />
       {/* <TestimonialOne /> */}
       <TestimonialTwo />
-      {/* <ExcellentWorkOne /> */}
-      {/* <BlogOne /> */}
+
       <FooterOne />
     </>
   );
